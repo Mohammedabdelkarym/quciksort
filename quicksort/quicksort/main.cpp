9c5b12e4addcabd2968f5a919a93a35c4aa2a8b3
mohammedabdelkarym <mohammed.abdelkarym@gmail.com>
2014-04-04 10:32:59 -0500

    Hello asdf

---
+++ quicksort/quicksort/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  quicksort
+//
+//  Created by Mohammed Abdelkarym on 4/4/14.
+//  Copyright (c) 2014 Mohammed Abdelkarym. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
